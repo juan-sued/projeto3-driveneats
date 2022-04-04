f8e081f8918bb3c79a3b3b9aadfb99f38bf7b09f
Juan <juansued19@gmail.com>
2022-04-04 13:00:17 -0300

    Ultima versão

--- scripts.js
+++ scripts.js
@@ -1,5 +1,56 @@
-function selecionado(card) {
-  const elemento = document
-    .querySelector("." + card)
-    .classList.toggle("selecionado");
+// === PRATO DOWN ===
+
+function selecionarPrato(prato) {
+  const comidaEscolhida = document.querySelector(".selecionado-prato");
+  if (comidaEscolhida !== null) {
+    comidaEscolhida.classList.remove("selecionado-prato");
+  }
+  prato.classList.add("selecionado-prato");
+
+  const pratoSelecionado = document
+    .querySelector(".prato")
+    .querySelector(".selecionado-prato");
+  console.log(pratoSelecionado);
+}
+// === PRATO UP ===
+
+// === BEBIDA DOWN  ===
+function selecionarBebidas(bebida) {
+  const comidaEscolhida = document.querySelector(".selecionado-bebida");
+  if (comidaEscolhida !== null) {
+    comidaEscolhida.classList.remove("selecionado-bebida");
+  }
+  bebida.classList.add("selecionado-bebida");
+
+  const bebidaSelecionada = document
+    .querySelector(".bebida")
+    .querySelector(".selecionado-bebida");
+  console.log(bebidaSelecionada);
+}
+// === BEBIDA UP ===
+
+// === SOBREMESA DOWN ===
+function selecionarSobremesa(sobremesa) {
+  // qual botão foi clicado
+  const comidaEscolhida = document.querySelector(".selecionado-sobremesa");
+
+  if (comidaEscolhida !== null) {
+    comidaEscolhida.classList.remove("selecionado-sobremesa");
+  }
+  sobremesa.classList.add("selecionado-sobremesa");
+
+  const sobremesaSelecionada = document
+    .querySelector(".sobremesa")
+    .querySelector(".selecionado-sobremesa");
+  console.log(sobremesaSelecionada);
+}
+// === SOBREMESA UP ===
+
+function liberarBotao() {
+  if (
+    pratoSelecionado !== null &&
+    bebidaSelecionada !== null &&
+    sobremesaSelecionada !== null
+  ) {
+  }
 }
